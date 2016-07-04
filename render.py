fa8b7317391007a366388bd2f9a4dc87407e11e2
Jonas Köritz <jonas.koeritz@gmail.com>
2016-07-04 16:47:35 +0200

    Skript kann jetzt mit Hilfe der Vorlage "Magnet.svg" Magneten für Gruppen, Züge, etc. erstellen

--- render.py
+++ render.py
@@ -3,7 +3,9 @@ from subprocess import call
 
 def render_raster_images(svg_file):
 	create_output_folders()
+
 	filename, ext = os.path.splitext(str(svg_file))
+	
 	call(["wkhtmltoimage", "--transparent", "--quality", "100", "--zoom", "4", "--crop-h", "1024", "--crop-w", "1024", str(svg_file), "./1024x1024/" + filename + ".png"])
 	
 	call(["magick", "./1024x1024/" + filename + ".png", "-resize", "50%", "./512x512/" + filename + ".png"])
@@ -28,24 +30,16 @@ def create_output_folders():
 def create_magnet_svg(svg_file):
 	with open('Magnet.svg', 'r') as magnet:
 		template = magnet.read()
-		with open(str(sys.argv[2]), 'r') as symbol:
+		with open(str(svg_file), 'r') as symbol:
 			symbol_data = symbol.read()
+			symbol_data = symbol_data.replace('<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">', "")
 			magnet_data = template.replace('%SYMBOL%', symbol_data)
+
 			filename, ext = os.path.splitext(svg_file)
 			with open(filename + " - Magnet.svg", 'w') as output_file:
 				output_file.write(magnet_data)
 
-if len(sys.argv) == 2:
-	render_raster_images(sys.argv[1])
-	
-elif len(sys.argv) == 3:
-	action = str(sys.argv[1]).lower()
-	print("Running action " + action + " on file '" + str(sys.argv[2]) + "'")
-	if action == "render":
-		render_raster_images(sys.argv[2])
-	elif action == "magnet":
-		create_magnet_svg(sys.argv[2])
-else:
+def render_all():
 	for file in glob.glob("*.svg"):
 		filename, ext = os.path.splitext(file)
 
@@ -60,4 +54,35 @@ else:
 		call(["optipng", "./256x256/" + filename + ".png"])
 		call(["optipng", "./128x128/" + filename + ".png"])
 
+def create_all_magnet_svg():
+	for file in glob.glob("*.svg"):
+		filename, ext = os.path.splitext(file)
+		filename = filename.lower()
+		if "magnet" not in filename and "führer" not in filename:
+			if "fgr" in filename:
+				print("Erzeuge Magnet-SVG fuer '" + file + "'")
+				create_magnet_svg(file)
+			elif "gruppe" in filename:
+				print("Erzeuge Magnet-SVG fuer '" + file + "'")
+				create_magnet_svg(file)
+			elif "zug" in filename:
+				print("Erzeuge Magnet-SVG fuer '" + file + "'")
+				create_magnet_svg(file)
+			elif "einheit" in filename:
+				print("Erzeuge Magnet-SVG fuer '" + file + "'")
+				create_magnet_svg(file)
 
+if len(sys.argv) == 3:
+	action = str(sys.argv[1]).lower()
+	print("Running action " + action + " on file '" + str(sys.argv[2]) + "'")
+	if action == "render":
+		render_raster_images(sys.argv[2])
+	elif action == "magnet":
+		create_magnet_svg(sys.argv[2])
+elif len(sys.argv) == 2:
+	action = str(sys.argv[1]).lower()
+	print("Running action " + action)
+	if action == "all-magnets":
+		create_all_magnet_svg()
+	elif action == "render-all":
+		render_all()
